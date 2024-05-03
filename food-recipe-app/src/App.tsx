25b0b9c1b84a784081ab9893913e2fb279a2cb4e
Cesar Whatley <julceswhat@gmail.com>
2024-05-02 20:22:35 -0500

    Added route structure

--- food-recipe-app/src/App.tsx
+++ food-recipe-app/src/App.tsx
@@ -1,10 +1,22 @@
 import "./App.css";
+import Navbar from "./components/navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Details from "./pages/details";
+import Favorites from "./pages/favorites";
 
 function App() {
   return (
-    <>
-      <div>Starigng code</div>
-    </>
+    <div>
+      <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/recipe-item/:id" element={<Details />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
