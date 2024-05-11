661614dc043029ef01555555a859ae2b3b801896
Cesar Whatley <julceswhat@gmail.com>
2024-05-11 00:57:21 -0500

    Completed product details

--- shopping-cart/src/pages/home.tsx
+++ shopping-cart/src/pages/home.tsx
@@ -1,4 +1,45 @@
+import { useEffect, useState } from "react";
+import { Circles } from "react-loader-spinner";
+import ProductTile, { IProduct } from "../components/product-tile";
+
 const HomePage = () => {
-  return <div>Home Page</div>;
+  const [product, setProduct] = useState<IProduct[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data = await response.json();
+        setProduct(data || []);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProduct();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <div className="min-h-screen w-full flex justify-center items-center">
+          <Circles
+            height={"120"}
+            width={"120"}
+            color={"rgb(127, 29, 29)"}
+            visible={true}
+          />
+        </div>
+      ) : (
+        <div className="min-h-[80vh] grid sm:grid-cols-2 md:grid-cols-3 space-x-5 space-y-10">
+          {product.map((product: IProduct) => (
+            <ProductTile key={product.id} product={product} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 export default HomePage;
